1aff80d9b9009541818ac7c4b6637a6bb6094231
jastigi <jastigi@gmail.com>
2026-06-14 11:53:20 +0200

    Add Campaign REST controller

---
+++ src/main/java/com/jastigi/silentcampaignmanager/controller/CampaignController.java
@@ -0,0 +1,40 @@
+package com.jastigi.silentcampaignmanager.controller;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.jastigi.silentcampaignmanager.entity.Campaign;
+import com.jastigi.silentcampaignmanager.service.CampaignService;
+
+@RestController
+@RequestMapping("/api/v1/campaigns")
+public class CampaignController {
+
+    private final CampaignService campaignService;
+
+    public CampaignController(CampaignService campaignService) {
+        this.campaignService = campaignService;
+    }
+
+    @PostMapping
+    public Campaign createCampaign(@RequestBody Campaign campaign) {
+        return campaignService.createCampaign(campaign);
+    }
+
+    @GetMapping
+    public List<Campaign> getAllCampaigns() {
+        return campaignService.getAllCampaigns();
+    }
+
+    @GetMapping("/{id}")
+    public Campaign getCampaignById(@PathVariable Long id) {
+        return campaignService.getCampaignById(id);
+    }
+
+}
